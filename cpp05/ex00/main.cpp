d566d442f00efee6bc6d3d4ee8132a543e776be2
nyykooo <nicholas.cakla@outlook.com>
2025-03-31 20:13:43 +0100

    cpp05/ex00: fixing problem where a throw out of a try block could happen

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 20:55:00 by ncampbel          #+#    #+#             */
-/*   Updated: 2025/01/22 14:09:22 by ncampbel         ###   ########.fr       */
+/*   Updated: 2025/03/31 19:42:09 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 int main(void)
 {
 	{
-		Bureaucrat *bureaucrat = new Bureaucrat("Nico", 99);
-		Bureaucrat *bureaucrat2 = new Bureaucrat("Top", 1);
-		Bureaucrat *bureaucrat3 = new Bureaucrat("Bottom", 150);
+		Bureaucrat *bureaucrat = NULL;
+		Bureaucrat *bureaucrat2 = NULL;
+		Bureaucrat *bureaucrat3 = NULL;
 
 		try
 		{
+			bureaucrat = new Bureaucrat("Nico", 99);
+			bureaucrat2 = new Bureaucrat("Top", 1);
+			bureaucrat3 = new Bureaucrat("Bottom", 150);
+			
 			bureaucrat->incrementGrade();
 			bureaucrat3->incrementGrade();
 			bureaucrat2->incrementGrade();
@@ -35,12 +39,16 @@ int main(void)
 	}
 
 	{
-		Bureaucrat *bureaucrat = new Bureaucrat("Nico", 99);
-		Bureaucrat *bureaucrat2 = new Bureaucrat("Top", 1);
-		Bureaucrat *bureaucrat3 = new Bureaucrat("Bottom", 150);
+		Bureaucrat *bureaucrat = NULL;
+		Bureaucrat *bureaucrat2 = NULL;
+		Bureaucrat *bureaucrat3 = NULL;
 
 		try
 		{
+			bureaucrat = new Bureaucrat("Nico", 99);
+			bureaucrat2 = new Bureaucrat("Top", 1);
+			bureaucrat3 = new Bureaucrat("Bottom", 150);
+			
 			bureaucrat->decrementGrade();
 			bureaucrat2->decrementGrade();
 			bureaucrat3->decrementGrade();
